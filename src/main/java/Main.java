68dc23bf597eec810d9d9bccd3df693b4f4f1bbc
Volition21 <Volition21@Hackion.com>
2015-10-14 23:38:43 +0800

    Ayyyy everything auto launches now.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,151 +14,172 @@ import java.util.zip.CheckedInputStream;
 
 public class Main {
 
-    public static final long	minecraft_server					= 280458825L; //Beta1.2_01
-    public static final long	minecraft_servero					= 2392702871L; //Beta1.2_01 processed w/ jarjar
-    public static final long	mysql								= 3001390372L; //mysql connector
-    public static final long	jarjar								= 233379283L; //jarjar decompiler
-    public static final long	rules								= 2575805698L; //rules for jarjar (For Beta1.2_01 obviously)
-	public static final long	VCRC								= 1411154803L; //version.txt
+	public static final long minecraft_server = 280458825L; // Beta1.2_01
+	public static final long minecraft_servero = 2392702871L; // Beta1.2_01
+																// processed w/
+																// jarjar
+	public static final long mysql = 3001390372L; // mysql connector
+	public static final long jarjar = 233379283L; // jarjar decompiler
+	public static final long rules = 2575805698L; // rules for jarjar (For
+													// Beta1.2_01 obviously)
+	public static final long VCRC = 1411154803L; // version.txt
 
-    public static final Logger log                                = Logger.getLogger("Minecraft");
+	private static final String URL = "http://hackion.com/mirror/Minecraft/B1.2_01/";
 
-    //TODO
-    //Change this.
-    public static void main(String[] args) throws IOException {
+	public static final Logger log = Logger.getLogger("Minecraft");
 
-        if (!fileExists("minecraft_servero.jar")) {
-            if (!fileExists("jarjar.jar") || !fileExists("rules.rules")) {
-                log("-----------------------------");
-                log("jarjar.jar and/or rules.rules are missing!");
-                log("-----------------------------");
-                System.exit(0);
-            }
-            checkCRC32("jarjar.jar", jarjar);
-            checkCRC32("rules.rules", rules);
+	// TODO
+	// Change this.
+	public static void main(String[] args) throws IOException {
 
-            if (!fileExists("minecraft_server.jar")) {
-                log("Missing minecraft_servero.jar, Downloading minecraft_server.jar...");
-                downloadFile("http://minecraft.net/download/minecraft_server.jar", "minecraft_server.jar");
-                checkCRC32("minecraft_server.jar", minecraft_server);
+		if (!fileExists("jarjar.jar")) {
+			log("jarjar.jar is missing, Downloading...");
+			downloadFile(URL + "jarjar.jar", "jarjar.jar");
+			checkCRC32("jarjar.jar", minecraft_server);
+			log("Loading jarjar.jar...");
+			dynamicLoadJar("jarjar.jar");
+			log("Finished downloading & loading jarjar.jar.");
+		}
 
-                log("Finished downloading minecraft_server.jar, start converting minecraft_server.jar to minecraft_servero.jar...");
-            } else
-                log("Missing minecraft_servero.jar, start converting minecraft_server.jar to minecraft_servero.jar...");
+		if (!fileExists("rules.rules")) {
+			log("rules.rules is missing, Downloading...");
+			downloadFile(URL + "rules.rules", "rules.rules");
+			checkCRC32("rules.rules", minecraft_server);
+			log("Finished downloading rules.rules.");
+		}
 
-            try {
-                com.tonicsystems.jarjar.Main.main(new String[] { "process", "rules.rules", "minecraft_server.jar", "minecraft_servero.jar" });
-            } catch (Throwable t) {
-                log.log(Level.SEVERE, null, t);
-            }
-            checkCRC32("minecraft_servero.jar", minecraft_servero);
+		if (!fileExists("minecraft_servero.jar")) {
+			if (!fileExists("minecraft_server.jar")) {
+				log("Vanilla server jar file is missing, Downloading...");
+				downloadFile(URL + "minecraft_server.jar", "minecraft_server.jar");
+				checkCRC32("minecraft_server.jar", minecraft_server);
+				log("Finished downloading minecraft_server.jar.");
+			} else
+				log("Creating minecraft_servero.jar now...");
 
-            log("Finished converting minecraft_server.jar, Starting minecraft server...");
-            dynamicLoadJar("minecraft_servero.jar");
-        } else
-            checkCRC32("minecraft_servero.jar", minecraft_servero);
+			try {
+				com.tonicsystems.jarjar.Main.main(
+						new String[] { "process", "rules.rules", "minecraft_server.jar", "minecraft_servero.jar" });
+			} catch (Throwable t) {
+				log.log(Level.SEVERE, null, t);
+			}
+			checkCRC32("minecraft_servero.jar", minecraft_servero);
+			log("minecraft_servero.jar successfully created.");
 
-        if (etc.getInstance().getDataSourceType().equalsIgnoreCase("mysql"))
-            checkCRC32("mysql-connector-java-bin.jar", mysql);
+			log("Loading minecraft server now...");
+			dynamicLoadJar("minecraft_servero.jar");
+		} else {
+			checkCRC32("minecraft_servero.jar", minecraft_servero);
+		}
 
-        if (checkForUpdate())
-            System.out.println("Update found.");
-        // derp.
+		if (etc.getInstance().getDataSourceType().equalsIgnoreCase("mysql")) {
+			checkCRC32("mysql-connector-java-bin.jar", mysql);
+		}
+		if (checkForUpdate()) {
+			System.out.println("Update found.");
+			// derp.
+		}
 
-        // My mod doesn't work with gui.
-        try {
-            net.minecraft.server.MinecraftServer.main(new String[] { "nogui" });
-        } catch (Throwable t) {
-            log.log(Level.SEVERE, null, t);
-        }
-        new DeadLockDetector();
-    }
+		// My mod doesn't work with gui.
+		try {
+			net.minecraft.server.MinecraftServer.main(new String[] { "nogui" });
+		} catch (Throwable t) {
+			log.log(Level.SEVERE, null, t);
+		}
+		new DeadLockDetector();
+	}
 
-    public static boolean fileExists(String filename) {
-        return new File(filename).exists();
-    }
+	public static boolean fileExists(String filename) {
+		return new File(filename).exists();
+	}
 
-    public static void checkCRC32(String fileName, long[] crcs) throws IOException {
-        if (etc.getInstance().getTainted())
-            return;
+	public static void checkCRC32(String fileName, long[] crcs) throws IOException {
+		if (etc.getInstance().getTainted()) {
+			return;
+		}
 
-        long checksum = getCRC32(fileName);
-        for (long i : crcs)
-            if (i == checksum)
-                return;
-        log("-----------------------------");
-        log(fileName + " does not match checksum!");
-        log("if you still want to run the server, delete version.txt to run the server in tainted mode.");
-        log("This means some of your files are either corrupted,outdated or to new(minecraft got updated?).");
-        log("-----------------------------");
-        System.exit(0);
+		long checksum = getCRC32(fileName);
+		for (long i : crcs) {
+			if (i == checksum)
+				return;
+		}
+		log("-----------------------------");
+		log(fileName + " does not match checksum!");
+		log("if you still want to run the server, delete version.txt to run the server in tainted mode.");
+		log("This means some of your files are either corrupted,outdated or to new(minecraft got updated?).");
+		log("You may also delete the file and the server will account for it.");
+		log("-----------------------------");
+		System.exit(0);
 
-    }
+	}
 
-    public static void checkCRC32(String fileName, long crc) throws IOException {
-        if (etc.getInstance().getTainted())
-            return;
+	public static void checkCRC32(String fileName, long crc) throws IOException {
+		if (etc.getInstance().getTainted()) {
+			return;
+		}
 
-        long checksum = getCRC32(fileName);
-        if (checksum != crc) {
-            log("-----------------------------");
-            log(fileName + " does not match checksum! Checksum found: " + checksum + ", required checksum: " + crc + ".");
-            log("This means some of your files are either corrupted,outdated or to new(minecraft got updated?).");
-            log("If you still want to run the server, delete version.txt to run the server in tainted mode.");
-            log("-----------------------------");
-            System.exit(0);
-        }
-    }
+		long checksum = getCRC32(fileName);
+		if (checksum != crc) {
+			log("-----------------------------");
+			log(fileName + " does not match checksum! Checksum found: " + checksum + ", required checksum: " + crc
+					+ ".");
+			log("This means some of your files are either corrupted,outdated or to new(minecraft got updated?).");
+			log("If you still want to run the server, delete version.txt to run the server in tainted mode.");
+			log("You may also delete the file and the server will account for it.");
+			log("-----------------------------");
+			System.exit(0);
+		}
+	}
 
-    public static long getCRC32(String fileName) throws IOException {
+	public static long getCRC32(String fileName) throws IOException {
 
-        FileInputStream stream = new FileInputStream(fileName);
-        CheckedInputStream cis = new CheckedInputStream(stream, new CRC32());
-        byte[] buf = new byte[128];
-        while (cis.read(buf) >= 0) {
-        }
+		FileInputStream stream = new FileInputStream(fileName);
+		CheckedInputStream cis = new CheckedInputStream(stream, new CRC32());
+		byte[] buf = new byte[128];
+		while (cis.read(buf) >= 0) {
+		}
 
-        long rt = cis.getChecksum().getValue();
-        stream.close();
-        cis.close();
+		long rt = cis.getChecksum().getValue();
+		stream.close();
+		cis.close();
 
-        return rt;
-    }
+		return rt;
+	}
 
-    public static void printCRC() throws IOException {
-        log("Minecraft_server CRC32: \t" + getCRC32("minecraft_server.jar"));
-        log("Minecraft_servero CRC32: \t" + getCRC32("minecraft_servero.jar"));
-        log("Jarjar CRC32: \t\t\t" + getCRC32("jarjar.jar"));
-        log("Rules CRC32: \t\t\t" + getCRC32("rules.rules"));
-        log("Mysql CRC32: \t\t\t" + getCRC32("mysql-connector-java-bin.jar"));
-    }
+	public static void printCRC() throws IOException {
+		log("Minecraft_server CRC32: \t" + getCRC32("minecraft_server.jar"));
+		log("Minecraft_servero CRC32: \t" + getCRC32("minecraft_servero.jar"));
+		log("Jarjar CRC32: \t\t\t" + getCRC32("jarjar.jar"));
+		log("Rules CRC32: \t\t\t" + getCRC32("rules.rules"));
+		log("Mysql CRC32: \t\t\t" + getCRC32("mysql-connector-java-bin.jar"));
+	}
 
-    public static void downloadFile(String website, String fileLocation) throws IOException {
-        URL url = new URL(website);
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream(fileLocation);
-        fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-        fos.close();
-    }
+	public static void downloadFile(String website, String fileLocation) throws IOException {
+		URL url = new URL(website);
+		ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+		FileOutputStream fos = new FileOutputStream(fileLocation);
+		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+		fos.close();
+	}
 
-    public static void log(String str) {
-        System.out.println(str);
-    }
+	public static void log(String str) {
+		System.out.println(str);
+	}
 
-    public static void dynamicLoadJar(String fileName) throws IOException {
-        URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        Class<?> sysclass = URLClassLoader.class;
-        try {
-            Method method = sysclass.getDeclaredMethod("addURL", new Class[] { URL.class });
-            method.setAccessible(true);
-            method.invoke(sysloader, new Object[] { (new File(fileName)).toURI().toURL() });
-        } catch (Throwable t) {
-            t.printStackTrace();
-            throw new IOException("Error, could not add URL to system classloader");
-        }
-    }
+	public static void dynamicLoadJar(String fileName) throws IOException {
+		URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		Class<?> sysclass = URLClassLoader.class;
+		try {
+			Method method = sysclass.getDeclaredMethod("addURL", new Class[] { URL.class });
+			method.setAccessible(true);
+			method.invoke(sysloader, new Object[] { (new File(fileName)).toURI().toURL() });
+		} catch (Throwable t) {
+			t.printStackTrace();
+			throw new IOException("Error, could not add URL to system classloader");
+		}
+	}
 
-    public static boolean checkForUpdate() {
-        return false;
-    }
+	public static boolean checkForUpdate() {
+		return false;
+	}
 }
